14566478a91be1ee9e8d04e7cd1435fc3e6a4367
Zahra Seyedi <za.seyedi@gmail.com>
2021-04-10 02:07:03 -0700

    creating work grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var timeDisplayEl = document.getElementById("time-display");
-
+var currentTime = parseInt(moment().format("H"));
+var savedEvents = [];
 
 // Date and Time
 displayTime = () => {
@@ -9,3 +10,32 @@ displayTime = () => {
   setInterval(displayTime, 1000);
 
 
+// work grid
+for (var i = 9; i <= 17; i++) {
+
+  var timeRowEl = $("<div>").addClass("row time-block");
+  var timeObj = moment().hour(i);
+  var timeText = moment(timeObj).format("dddd hA");    
+  if (i == 17) {
+      var timeBlockEl =  $("<div>").addClass("col-2 col-md-1 hour").attr("style", "border-bottom: 1px dashed #000000").text(timeText);
+  } else {
+      var timeBlockEl = $("<div>").addClass("col-2 col-md-1 hour").text(timeText);
+  }
+
+
+  (i < currentTime) ? highlightClass = "past" : (i == currentTime) ? highlightClass = "present" : highlightClass = "future";
+  
+  var eventBlock = $("<textarea>").addClass("col-8 col-md-10 description eventBlock " + highlightClass);
+
+  
+  loadSavedEvents();
+  $.each(savedEvents, (index, savedEventItem) =>{
+      (savedEventItem.time == timeText) ? eventBlock.text(savedEventItem.event) : "";
+      });
+
+  var saveBtn = $("<div>").addClass("col-2 col-md-1 saveBtn p-4").html('<i class="far fa-save"></i>');
+
+  timeRowEl.append(timeBlockEl, eventBlock, saveBtn);
+
+  $(".container").append(timeRowEl);
+};
